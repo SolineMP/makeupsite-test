9eedccde020116585a5865cc8bea271002212e64
ISAsxm <huguesisabelle82@gmail.com>
2019-10-24 17:19:01 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Récupération de l'adresse mail pour la newsletter */
+
+document.getElementById("myInput").addEventListener("click", function() {
+	alert("Nous avons bien reçu votre email!")
+});
+
+
